2816b1045ce5d8584f50baea2882732cb16daa32
wictor <wictor@hotmail.com.br>
2018-10-02 10:18:59 -0300

    Configuração da pagina do pdf e uma pequena gambiara para conseguir converter qualquer extensão sem erro

--- ConvertTextToPdf.py
+++ ConvertTextToPdf.py
@@ -1,6 +1,27 @@
 import pdfkit
+import os
+
+options = {
+    'page-size': 'Letter',
+    'margin-top': '0.75in',
+    'margin-right': '0.75in',
+    'margin-bottom': '0.75in',
+    'margin-left': '0.75in',
+    'encoding': "UTF-8",
+    'custom-header' : [
+        ('Accept-Encoding', 'gzip')
+    ],
+    'cookie': [
+        ('cookie-name1', 'cookie-value1'),
+        ('cookie-name2', 'cookie-value2'),
+    ],
+    'no-outline': None,
+    #'load-error-handling': 'ignore'
+}
 
 def textToPdf(source, destiny):
+    os.rename(source, source + '.txt')
     path_wkthmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
     config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
-    pdfkit.from_url(source, destiny, configuration=config)
+    pdfkit.from_file(source + '.txt', destiny, configuration=config, options=options)
+    os.rename(source + '.txt', source)
